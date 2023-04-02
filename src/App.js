bc9e324cb6a3687ff1cae20512df7485dfcefca1
dav1lex <zedxurl@gmail.com>
2023-04-02 12:53:18 +0200

    added logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import {logDOM} from "@testing-library/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchitem, setsearchitem] = useState('')
+    const [templist,setTemplist] = useState([])
+    const SEARCH_URI = `https://api.coingecko.com/api/v3/search?query=${searchitem}`
+
+    function inputhandler(ev) {
+        setsearchitem(ev.target.value)
+    }
+
+    function searchcoin() {
+        fetch(SEARCH_URI).then(r => r.json()).then(json =>
+            setTemplist(json.coins)
+        )
+        console.log(templist)
+        setsearchitem('')
+    }
+
+    return (
+        <div className="w-screen h-screen bg-gray-500">
+            <input onChange={inputhandler}/>
+            <button onClick={searchcoin}>search</button>
+           <div className='flex flex-wrap'>
+               {templist.map((coin)=>{
+                   return <div className='bg-yellow-300 w-52 h-24  m-5'>
+                       <h3>{coin.name}</h3>
+                       <img src={coin.large} width={50}/>
+
+                   </div>
+               })}
+           </div>
+        </div>
+    );
 }
 
 export default App;
